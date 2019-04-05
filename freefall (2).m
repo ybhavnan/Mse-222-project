41949faca655789e744fed1d77a92d282e6f3e29
Khoideptrai123 <38174809+Khoideptrai123@users.noreply.github.com>
2019-04-05 13:34:23 -0700

    Update freefall (2).m

--- freefall (2).m
+++ freefall (2).m
@@ -1,16 +1,15 @@
 %Free fall function
-function [result2] = freefall()
+function [result2] = freefall(h)
 global GlobalXYT;
 x0 = GlobalXYT(end,1);
 y0 = GlobalXYT(end,2);
 
 %For this part I assume the velocity to be V= (change of distance)/(change
 %of time)
-initialVelX=(GlobalXYT(end,1)-GlobalXYT(end-2,1))/(GlobalXYT(end,3)-GlobalXYT(end-2,3));
+initialVelX=0;
 initialVelY=(GlobalXYT(end,2)-GlobalXYT(end-2,2))/(GlobalXYT(end,3)-GlobalXYT(end-2,3));
 
 g = 9.81; 
-h = 0.1 ;    % assume h = 0.1m is the height of free falling
 fall_time = sqrt(2*h/g);
 t2 = 0:0.001:fall_time;
 x = x0+ initialVelX*t2;
