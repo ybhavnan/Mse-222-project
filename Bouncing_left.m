0c16900459b6338a5308fe0c4d4bfcfb33c62a65
Khoideptrai123 <38174809+Khoideptrai123@users.noreply.github.com>
2019-04-06 21:20:11 -0700

    This is for a bouncing motion for the ball in the left side ramp

---
+++ Bouncing_left.m
@@ -0,0 +1,44 @@
+function[result] = Bouncing_left(angle, c) % this is the same as bouncing motion, however it applies for the left side ramp
+global GlobalXYT;
+g=9.81;
+localtime = 0;
+timejump = 0.001; %Time step size is 1 ms
+t= 0.000;
+stop = false;
+angle = deg2rad(angle); %slope angle
+Vx = -c*abs(((GlobalXYT(end,1) - GlobalXYT(end-1,1))/(GlobalXYT(end,3) - GlobalXYT(end-1,3))));%velocity after impact in x direction
+Vy = c*abs(((GlobalXYT(end,2) - GlobalXYT(end-1,2))/(GlobalXYT(end,3) - GlobalXYT(end-1,3))));% velocity after impact in y -direction
+velocity = sqrt(Vx.^2 + Vy.^2); %calculating the initial velocity from the last element data
+x0=GlobalXYT(end,1);
+y0= GlobalXYT(end,2);
+t0 = GlobalXYT(end,3);
+x=x0;
+y=y0;
+result = [x0' y0' t0'];
+%Real contact will be acot(vx/vy) + 2*angle, not the one below.
+bounce_angle= acot(abs((Vx/Vy)));%Contact angle change after each bounce.
+landingtime = abs((2*velocity*sin(bounce_angle))/(g*cos(angle))); %Total time to complete 1 bounce
+
+while (stop == false)
+    if (localtime < landingtime) %This check whether or not a bounce is completed and move to a new bounce.
+        localtime = localtime + timejump %Local time of each bounce. t is the temp time (only use inside this function).
+        t= t + timejump
+        x = Vx*localtime + x0
+        y = Vy*localtime - 0.5*g*localtime^2 + y0
+        result = [result; [x' y' (t0+t)']];  %Output.
+    else
+        localtime=0;
+        x0=x;
+        y0=y;
+        Vy = abs((Vy - g*landingtime))*c;
+        Vx = Vx*c;
+        velocity=(sqrt((Vx.^2) + (Vy.^2))); %New bounce velocity
+        bounce_angle = acot(Vx/Vy); %Contact angle change after each bounce.
+        landingtime = abs((2*velocity*sin(bounce_angle+angle))/(g*cos(angle)));%Total time to complete 1 bounce
+    end
+    if ((bounce_angle - angle) <= 0)
+        stop = true;
+        break; %Exit the function if the contact angle become too close to slope angle.
+    end
+end
+end
