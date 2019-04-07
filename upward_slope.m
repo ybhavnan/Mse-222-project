1d32a0a30183de4f10157010e02c009555d31a37
Khoideptrai123 <38174809+Khoideptrai123@users.noreply.github.com>
2019-04-06 21:19:07 -0700

    This is for the ball going up the ramp until its velocity is 0

--- upward_slope.m
+++ upward_slope.m
@@ -8,30 +8,29 @@ y0 = GlobalXYT(end,2);
 
 g= 9.81;
 Coeff = muy;
-angle = -a*pi/180;
+angle = a*pi/180;
 
-dtime = 0.01;
+dtime = 0.001;
 initialVelX=(GlobalXYT(end,1)-GlobalXYT(end-2,1))/(GlobalXYT(end,3)-GlobalXYT(end-2,3));
 initialVelY=(GlobalXYT(end,2)-GlobalXYT(end-2,2))/(GlobalXYT(end,3)-GlobalXYT(end-2,3));
-vi = sqrt(initialVelX.^2+initialVelY.^2);
+vi = initialVelY*sin(angle)- initialVelX*cos(angle);
 
-
-acc = g*(sin(angle)- Coeff*cos(angle)); %acceleration
-
+ 
+%acc = g*(sin(angle)- Coeff*cos(angle)); %acceleration
+acc = -5*g*sin(angle)/7;
 %V_magnitude = sqrt(vi^2 + 10*g*sin(angle)/7);
 
 %Solving for time
 syms T;
-eqn = 0 == acc*T + vi;
+eqn = 0 == acc*T + vi; % the ball goes up on the hill until the velocity is 0
 solT = double(solve(eqn,T));
 calculatedT = solT(solT>0);
 calculatedT = min(calculatedT);
 t = 0: dtime: calculatedT;
 d = vi * t + 0.5 * acc * t.^2;
 x = x0 - d * cos(angle);
-y = y0 - d * sin(angle);
+y = y0 + d * sin(angle);
 time = GlobalXYT(end,3) + t;
 result1 = [x' y' time' ];
 
 end
-
